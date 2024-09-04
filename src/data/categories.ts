7e00523e6a830e022c53c0d5a3de78f284fb0435
WayneGoosen <13494899+WayneGoosen@users.noreply.github.com>
2024-09-04 22:47:15 +0200

    fix: escape chars and add category

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,2 +1,2 @@
 // List of categories for blog posts, Development, CI/CD
-export const CATEGORIES = ['DevOps', 'Containers', 'CICD', 'IaC'] as const
+export const CATEGORIES = ['DevOps', 'Containers', 'CICD', 'IaC', 'Platform Engineering'] as const
